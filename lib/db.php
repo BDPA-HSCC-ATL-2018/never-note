4f191b4e7665adf18ba676e4877b0c7cd50c751b
Eddie Gichuhi <gichedw@gmail.com>
2018-05-05 11:55:37 -0400

    Updating DB connection library

--- lib/db.php
+++ lib/db.php
@@ -2,7 +2,6 @@
 $conn = new mysqli('localhost', 'root', '','noteapp');
 if ($conn->connect_error) {
   echo "<div class='alert alert-dark'>There was a problem connecting to the databse.</div>";
-} else {
-
+  return;
 }
 ?>
